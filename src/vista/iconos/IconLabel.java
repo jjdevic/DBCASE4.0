a886c10f3dafbd42bc179af42e3581c3d52390ce
Javier de Vicente <jdevic01@ucm.es>
2023-09-26 20:54:38 +0200

    project switched to Maven

---
+++ src/vista/iconos/IconLabel.java
@@ -0,0 +1,24 @@
+package vista.iconos;
+
+import vista.tema.Theme;
+
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class IconLabel extends JLabel{
+	private Theme theme = Theme.getInstancia();
+	public IconLabel(Icon icon){
+		super(icon);
+		setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+	}
+	
+	public IconLabel(Icon icon, String text) {
+		super(icon);
+		setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		setText(text);
+		setFont(theme.font());
+		setVerticalTextPosition(JLabel.BOTTOM);
+		setHorizontalTextPosition(JLabel.CENTER);
+	}
+}
---
+++ src/vista/imagenes/ImagePath.java
@@ -0,0 +1,9 @@
+package vista.imagenes;
+
+public class ImagePath {
+		public static final String LOGODBDT = "vista/imagenes/DBCase_logo.png";
+		public static final String LOGOFDI = "vista/imagenes/fdi.gif";
+		public static final String IMAGE1 = "vista/imagenes/ConceptualSchema.png";
+		public static final String IMAGE2 = "vista/imagenes/LogicalSchema.png";
+		public static final String IMAGE3 = "vista/imagenes/PhysicalSchema.png";
+}
